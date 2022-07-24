c1c69ff91f2cb12478c2973d6ba52a4214697a9f
Abyou <emandaabyou@gmail.com>
2022-07-24 18:24:17 -0500

    Updates

---
+++ precache-manifest.041ba5df61f0e91d43a6225bb0741059.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b39d7164f6e65c71c12",
+    "url": "/react-portfolio-app/static/css/main.baa40436.chunk.css"
+  },
+  {
+    "revision": "0b39d7164f6e65c71c12",
+    "url": "/react-portfolio-app/static/js/main.0b39d716.chunk.js"
+  },
+  {
+    "revision": "9f6f6cec55adb720d38c",
+    "url": "/react-portfolio-app/static/js/1.9f6f6cec.chunk.js"
+  },
+  {
+    "revision": "dfdfabc79b1c7d9303a2",
+    "url": "/react-portfolio-app/static/css/2.cfbb3276.chunk.css"
+  },
+  {
+    "revision": "dfdfabc79b1c7d9303a2",
+    "url": "/react-portfolio-app/static/js/2.dfdfabc7.chunk.js"
+  },
+  {
+    "revision": "eca2f40b86977d883627",
+    "url": "/react-portfolio-app/static/js/runtime~main.eca2f40b.js"
+  },
+  {
+    "revision": "082cd92619a4b9699e1a8a381eaaab73",
+    "url": "/react-portfolio-app/static/media/background.082cd926.png"
+  },
+  {
+    "revision": "5c832cea2970eefebea8c818a083832a",
+    "url": "/react-portfolio-app/static/media/project-1.5c832cea.png"
+  },
+  {
+    "revision": "03b69a4b298ffa19c0282e8448bac8f2",
+    "url": "/react-portfolio-app/static/media/project-2.03b69a4b.png"
+  },
+  {
+    "revision": "9974e04772180d47e753ccd7d7d1da7e",
+    "url": "/react-portfolio-app/static/media/project-3.9974e047.png"
+  },
+  {
+    "revision": "0e845996429e07c8d55cb1b9b7ab89ff",
+    "url": "/react-portfolio-app/static/media/project-4.0e845996.png"
+  },
+  {
+    "revision": "6ee1057fc4f4285c2609dcf4ce3d487b",
+    "url": "/react-portfolio-app/static/media/project-5.6ee1057f.png"
+  },
+  {
+    "revision": "849c1d314a2d9eda9b7f993130bdf4d4",
+    "url": "/react-portfolio-app/static/media/project-6.849c1d31.png"
+  },
+  {
+    "revision": "550f065495237ab7b14c676a5047a7e5",
+    "url": "/react-portfolio-app/index.html"
+  }
+];
